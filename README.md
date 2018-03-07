eae5b29386a314ff945c88f9d0a738f7706cc8a1
Anthony Gubler <agubler@sitepen.com>
2018-03-07 21:08:13 +0000

    Initial stateful implementation

---
+++ README.md
@@ -0,0 +1,55 @@
+# dojo2-stateful
+
+A simple state lib for Dojo 2 inspired by [Unstated](https://github.com/jamiebuilds/unstated).
+
+## How do I use this package?
+
+```
+npm install dojo2-stateful
+```
+
+## Features
+
+Simple state mechanism that enables sharing state across your application widget tree without needing to introduce complex state management.
+
+Simple Counter Example:
+
+```ts
+interface CounterState {
+	count: number
+};
+
+class CounterContainer extends Container<CounterState> {
+	state = {
+	  count: 0
+	};
+
+	increment = () => {
+	  this.setState({ count: this.state.count + 1 });
+	}
+
+	decrement = () => {
+	  this.setState({ count: this.state.count - 1 });
+	}
+}
+
+class Counter extends ProjectorMixin(WidgetBase) {
+	protected render() {
+		return w(Subscribe, { to: [CounterContainer], render: (counter: CounterContainer) => {
+			return v('div', [
+				v('button', { onclick: counter.decrement }, [ 'Decrement' ]),
+				v('span', [ `${counter.state.count}` ]),
+				v('button', { onclick: counter.increment }, [ 'Increment' ])
+			])
+		}})
+	}
+}
+
+const registry = new Registry();
+// create state provider for the registry
+createStatedProvider(registry);
+
+const counter = new Counter();
+counter.setProperties({ registry });
+counter.append();
+```
---
+++ src/index.ts
@@ -0,0 +1,99 @@
+import Symbol from '@dojo/shim/Symbol';
+import Evented from '@dojo/core/Evented';
+import Map from '@dojo/shim/Map';
+import WidgetBase from '@dojo/widget-core/WidgetBase';
+import inject from '@dojo/widget-core/decorators/inject';
+import Registry from '@dojo/widget-core/Registry';
+import Injector from '@dojo/widget-core/Injector';
+import { Constructor, VNode } from '@dojo/widget-core/interfaces';
+
+const STATEFUL_KEY = Symbol();
+
+export class StatefulInjector extends Evented {
+	private _containerMap = new Map();
+
+	getInstance = (Container: Constructor<Container>) => {
+		return this._containerMap.get(Container);
+	};
+
+	addInstance = (Container: Constructor<Container>, container: Container) => {
+		this._containerMap.set(Container, container);
+	};
+}
+
+export function createStatefulProvider(registry: Registry) {
+	const statefulInjector = new StatefulInjector();
+	const injector = new Injector(statefulInjector);
+	registry.defineInjector(STATEFUL_KEY, injector);
+}
+
+export abstract class Container<S = {}> {
+	private _listeners: Function[] = [];
+	protected abstract state: S;
+
+	public setState(state: Partial<S>) {
+		this.state = { ...(this.state as any), ...(state as any) };
+		this.invalidate();
+	}
+
+	public register = (fn: Function) => {
+		if (this._listeners.indexOf(fn) === -1) {
+			this._listeners.push(fn);
+		}
+		return {
+			destroy: () => {
+				const index = this._listeners.indexOf(fn);
+				if (index !== -1) {
+					this._listeners.splice(index, 1);
+				}
+			}
+		};
+	};
+
+	public invalidate = () => {
+		this._listeners.forEach((listener: Function) => {
+			listener();
+		});
+	};
+}
+
+export interface SubscribeProperties {
+	to: Constructor<Container>[];
+	render(...c: Container[]): VNode;
+}
+
+@inject({
+	name: STATEFUL_KEY,
+	getProperties: (inject: StatefulInjector, properties) => {
+		return {
+			to: properties.to,
+			render: properties.render,
+			getInstance: inject.getInstance,
+			addInstance: inject.addInstance
+		};
+	}
+})
+export class Subscribe extends WidgetBase<SubscribeProperties> {
+	private _invalidate = this.invalidate.bind(this);
+
+	private _createContainer() {
+		const { to, getInstance, addInstance } = this.properties as any;
+		const containers: Constructor<Container>[] = [];
+		to.forEach((Container: Constructor<Container>) => {
+			let container = getInstance(Container);
+			if (container) {
+				containers.push(container);
+			} else {
+				container = new Container();
+				containers.push(container);
+				addInstance(Container, container);
+			}
+			container.register(this._invalidate);
+		});
+		return containers;
+	}
+
+	render() {
+		return this.properties.render.apply(null, this._createContainer());
+	}
+}
